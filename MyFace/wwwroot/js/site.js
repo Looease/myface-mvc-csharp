ba9a1735664dcf947c1b4c0364c1554793de4b51
Louise <louise.gilligan@zoopla.co.uk>
2020-07-29 16:44:20 +0100

    Tidied up JS

--- MyFace/wwwroot/js/site.js
+++ MyFace/wwwroot/js/site.js
@@ -1,18 +1,14 @@
-
-
+const element = document.getElementById("mobile-menu");
 
 
 document.getElementById("mobile-menu-button").addEventListener("click", function()
 {
-    let element = document.getElementById("mobile-menu");
     if(element.classList.contains("open")){
         element.classList.remove("open");
     }else{
         element.classList.add("open");
-    }
- 
+    } 
 });
-let element = document.getElementById("mobile-menu");
 
 document.getElementById("menu-overlay").addEventListener("click", function(){
     element.classList.remove("open");
