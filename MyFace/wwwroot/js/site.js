bdcc6a559e314e7c7f9ac1bd434b3e8310019f80
Louise <louise.gilligan@zoopla.co.uk>
2020-07-29 13:52:02 +0100

    Some Javascript

---
+++ MyFace/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+ //your code
+});
+
+let header = document.getElementById("posts-section");
+
+header.addEventListener("click", function(){ header.style.display="none" });
